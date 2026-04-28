f404180b21ffa7356e3a0069acee8ed41d9b6e9e
parasssssss <156409035+parasssssss@users.noreply.github.com>
2026-04-28 20:01:13 +0530

    Refactor main.py to improve readability
    Removed unnecessary comments and simplified print statement.

--- main.py
+++ main.py
@@ -7,19 +7,19 @@ from scripts.db_operations import connect_db, create_table, upsert_comments
 
 
 def main():
-    # 🔍 Take keyword input (dynamic now)
+    
     keyword = input("Enter keyword (default: iPhone): ") or "iPhone"
 
-    # 1️⃣ Fetch comments + video data
+    
     raw_df = fetch_comments(keyword=keyword, max_videos=5, max_comments=100)
     
-    # 2️⃣ Clean comments
+  
     cleaned_df = clean_comments(raw_df)
     
-    # 3️⃣ Analyze engagement
+    
     analyzed_df = analyze_engagement(cleaned_df)
     
-    # 4️⃣ PostgreSQL
+    
     conn, cursor = connect_db()
     
     create_table(cursor, conn)
@@ -28,7 +28,7 @@ def main():
     cursor.close()
     conn.close()
 
-    print("✅ Pipeline completed successfully!")
+    print("Pipeline completed successfully!")
 
 
 if __name__ == "__main__":
